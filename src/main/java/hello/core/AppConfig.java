1a1c68e1de8b5e8ea1978fc870b9114212eb9243
nhwa <44343908+nhwa@users.noreply.github.com>
2022-06-01 18:36:30 +0900

    Change AppConfig to Spring-based

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,24 +8,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // DI : 의존관계 주입
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         // AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
         // AppConfig는 생성한 객체 레퍼런스를 생성자를 통해 주입
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
