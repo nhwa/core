def9e8484b9fb3f2b7a3b2401d55f86a80b6faac
nhwa <44343908+nhwa@users.noreply.github.com>
2022-06-01 16:13:55 +0900

    Add remark about Impl

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.member;
 
+// 인터페이스에 해당하는 구현체가 하나면 Impl로 끝나도록 네이밍함
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,6 +1,5 @@
 package hello.core.member;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
