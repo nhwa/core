88b8669411693ff627a99b343dd516a6611c4dba
nhwa <44343908+nhwa@users.noreply.github.com>
2022-06-01 18:33:16 +0900

    Replace implementation class

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -27,6 +27,7 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
