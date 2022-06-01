250717b9328f010fb0da0671b9fe2d7a50f3e938
nhwa <44343908+nhwa@users.noreply.github.com>
2022-06-01 17:41:58 +0900

    Refactoring AppConfig

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
@@ -14,10 +15,18 @@ public class AppConfig {
     public MemberService memberService(){
         // AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
         // AppConfig는 생성한 객체 레퍼런스를 생성자를 통해 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
