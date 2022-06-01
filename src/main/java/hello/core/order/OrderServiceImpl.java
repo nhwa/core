ef65ad82c750c34ca6fa24b0a9c2fccf6794fc13
nhwa <44343908+nhwa@users.noreply.github.com>
2022-06-01 16:17:08 +0900

    Create OrderService, OrderServiceImpl

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+    private final MemberRepository memoryMemberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memoryMemberRepository.findById(memberId);
+
+        // SRP 준수(단일책임원칙) : 하나의 클래스에서 하나의 책임만 주문쪽에서는 주문만 고침!
+       int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice );
+    }
+}
