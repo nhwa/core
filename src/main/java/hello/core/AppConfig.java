2baf5ce0627788ba0f4008351c8d6443cdd7334d
nhwa <44343908+nhwa@users.noreply.github.com>
2022-06-16 12:40:44 +0900

    Create ConfigurationSingletonTest

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -22,16 +22,20 @@ public class AppConfig {
     public MemberService memberService(){
         // AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
         // AppConfig는 생성한 객체 레퍼런스를 생성자를 통해 주입
+
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
