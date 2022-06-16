f1bc024ef08be6ef70bbf43dd8a719e0734ffba6
nhwa <44343908+nhwa@users.noreply.github.com>
2022-06-16 11:28:50 +0900

    Create SingletonTest

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -1,2 +1,32 @@
-package hello.core.beandefinition;public class BeanDefinitionTerst {
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+//    factory bean 방식
+//    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+//  직접 스프링 빈을 등록하는 방법
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() ==  beanDefinition.ROLE_APPLICATION){
+                System.out.println("beanDefinitionName = "+ beanDefinitionName + ", beanDefinition = "+ beanDefinition);
+            }
+        }
+    }
 }
+
