0db55e4e8159988c7ec9565ce926d66849728326
nhwa <44343908+nhwa@users.noreply.github.com>
2022-06-17 12:18:41 +0900

    Apply Lombok

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,9 +5,11 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/*
 @Component
 public class OrderServiceImpl implements OrderService{
 //    private final MemberRepository memoryMemberRepository = new MemoryMemberRepository();
@@ -67,4 +69,32 @@ public class OrderServiceImpl implements OrderService{
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
+}
+*/
+
+
+@Component
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // RequiredArgsConstructor => 생성자 자동 생성
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+
+        // SRP 준수(단일책임원칙) : 하나의 클래스에서 하나의 책임만 주문쪽에서는 주문만 고침!
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice );
+    }
+
+
 }
