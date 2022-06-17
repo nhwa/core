43324d938cb0034ad6dbfe67ecd8269b746af202
nhwa <44343908+nhwa@users.noreply.github.com>
2022-06-17 12:05:32 +0900

    Create OrderServiceImpl using Constructor Injection

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -14,41 +14,44 @@ public class OrderServiceImpl implements OrderService{
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
-    //final : 무조건 값이 있어야 한다 => 생성자 호출
-//    private final MemberRepository memberRepository;
-//    private final DiscountPolicy discountPolicy;
+    // final : 무조건 값이 있어야 한다 => 생성자 호출 - 1) 생성자 주입
+    // ==> final을 통해 생성ㄹ장에서 값이 설정되지 안는 오류를 컴파잃시점에서 체크할 수 있음
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    // 선택적 의존관계시 생성할때 값이 있어햐하는 final은 사용x
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    // 선택적 의존관계시 생성할때 값이 있어햐하는 final은 사용x - 2) 수정자 주입, 4) 메서드 주입
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
 
-    // 필드 주입
+    //  - 3) 필드 주입
 //    @Autowired private DiscountPolicy discountPolicy;
 //    @Autowired private MemberRepository memberRepository;
 
-    // 생성자가 1개일때는 @Autowired 생략 가능!
-//    @Autowired
+    // 생성자가 1개일때는 @Autowired 생략 가능! - 1) 생성자 주입
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
-    // 선택적 의존관계
-//    @Autowired(required = false)
+    // 2) 수정자 주입
+//    @Autowired
 //    public void setMemberRepository(MemberRepository memberRepository) {
 //        this.memberRepository = memberRepository;
 //    }
+//
 //    @Autowired
 //    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
 //        this.discountPolicy = discountPolicy;
 //    }
 
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy
-            discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // 4) 일반 메서드 주입
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy
+//            discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
